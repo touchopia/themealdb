9d03d913f036c14920f4d75e9cd6318acc4508c0
Phil Wright <touchopia@me.com>
2022-05-01 12:08:46 -0600

    removing tests

--- themealdbTests/MealAPIClientTests.swift
+++ themealdbTests/MealAPIClientTests.swift
@@ -9,18 +9,5 @@ import XCTest
 @testable import themealdb
 
 class MealAPITests: XCTestCase {
-
-    func test_init_fetchMealsList() {
-        
-        let apiClent = APIClient()
-        let viewModel = MealListViewModel(apiClient: apiClent)
-        
-        var meals = [Meal]()
-        
-        viewModel.loadMealsList { mealsArray in meals = mealsArray }
-        
-        // Assert that the asynchronous task worked.
-        XCTAssertTrue(meals.count > 0)
-    }
     
 }
